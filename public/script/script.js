3081ee6686d058a21bab3070b5bb805ad70b6f29
Stranger1002 <ozodzonsiddikov@exaple.com>
2024-04-24 18:37:42 +0500

    first commit

---
+++ public/script/script.js
@@ -0,0 +1,62 @@
+// +, -, *, /
+
+// console.log(1 + 2); // 3
+// console.log(1 - 2); // -1
+// console.log(1 * 2); // 2
+// console.log(1 / 2); // 0.5
+
+// // **, %
+// console.log(2 ** 4) // 16
+// console.log(4 % 3); // 1
+
+// let a = '45';
+// let b = 45;
+
+// console.log(b + a); // 4545
+
+// console.log('45' == 45); // true
+// console.log('42' === 45); // false
+// console.log(76 == 45); // false
+// console.log(72 === 42); // false
+// console.log('76' === '76'); // true
+// console.log('76' === 76); // false
+
+
+// Math
+
+// const a = Math.round(45.8);
+// const b = Math.floor(45.9);
+// const c = Math.ceil(45.1);
+// const d = Math.pow(2, 5);
+// const e = Math.random() * 100;
+// const j = Math.floor(Math.random() * (300 - 100 + 1) + 100);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(Math.trunc(e));
+// console.log(j);
+
+// console.log(Math.abs(56 - 66));
+
+// const age = prompt('Введите возраст');
+// const isMarried = confirm('Вам есть 18');
+// const abc = alert('Продолжать ?');
+// console.log(age);
+// console.log(isMarried);
+
+// ///////////////////////////////////////////////////
+// const totalBalance = prompt('Balance');
+// const income = prompt('Income');
+// const expense = prompt('expense');
+
+// const incomePer = (Number(income) / Number(totalBalance)) * 100;
+// const expensePer = (Number(expense) / Number(totalBalance)) * 100;
+
+// alert(`
+// Your balance: ${totalBalance};
+// Your income percentage: ${incomePer}%;
+// Your expense percentage: ${expensePer}%;
+// `)
+
+// console.log(Number('232dsd'));
